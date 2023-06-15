2245fdfa16465bef79ba5edea57d20732c5f0ba3
itsonlythebeginning <107440223+itsonlythebeginning@users.noreply.github.com>
2023-06-16 00:37:37 +0300

    Update script.js
    add vh from js to css

--- script.js
+++ script.js
@@ -135,3 +135,16 @@ function burgerClose() {
 
 
 
+let vh = window.innerHeight * 0.01;
+
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+
+
+
